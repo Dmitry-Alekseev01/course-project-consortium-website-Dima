fa417108d37be92a9f811dc95f0e7684e3e1e7ab
leonidlivshits <Leolivshitsss@yandex.ru>
2025-03-23 15:55:02 +0300

    error fixed

--- my-app/src/pages/Publications/Publications.jsx
+++ my-app/src/pages/Publications/Publications.jsx
@@ -11,7 +11,7 @@ const Publications = () => {
   useEffect(() => {
     const fetchPublications = async () => {
       try {
-        const response = await fetch ("http://94.158.219.154:5000/api/publications")//await fetch('http://127.0.0.1:5000/api/publications');
+        const response = await fetch('http://127.0.0.1:5000/api/publications');
         const data = await response.json();
         setPublications(data);
       } catch (error) {
